5db3402beb6c348e3c84edb4746deb92db5d4492
supr3mion <s04022687@gmail.com>
2024-01-10 10:35:09 +0100

    setup function overhaul
    added:
    - function for connecting to Wi-Fi
    - function for connecting to broker
    
    enabled:
    - MQTT message receive function
    - MQTT poll function
    
    notes:
    - since both the MQTT and arduino ron on the same local network the system will try to connect to the broker using the gateway IP instead of a pre-defined IP or url
    - the wifi had problems establishing a reliable connection, this is now resolved by forcing it to wait for a gateway IP from the DHCP server

--- src/main.cpp
+++ src/main.cpp
@@ -25,14 +25,75 @@ char mqtt_pass[] = SECRET_MQTT_PASS;
 WiFiClient wifiClient;
 MqttClient mqttClient(wifiClient);
 
-// Required broker information, includes ip and port of the broker itself
-// Also includes the subscribed topic
-const char broker[] = SECRET_MQTT_IP; //IP address of the eclipse broker.
-int        port     = 1883;
-const char subscribe_topic[]  = "/IO"; // Topic for sending information to subscribed devices
-
 // Define class, this function is build after the loop function
 void eclipseReceived(int messageSize);
+
+// Global connection status, by default false upon running code
+bool connected = false;
+// Function for connecting arduino to Wi-Fi
+void connectWifi() {
+    // Check arduino EPS32 firmware version
+    String fv = WiFi.firmwareVersion();
+    if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+        Serial.println("Please upgrade the firmware");
+        while (1);
+    }
+
+    // Attempt to connect to Wi-Fi connection using the provided credentials
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+    while (WiFi.begin(ssid, pass) != WL_CONNECTED) {
+        // Failed to connect, retry
+        Serial.print(".");
+        delay(5000);
+    }
+
+    Serial.println("You're connected to the network");
+    Serial.println();
+
+    Serial.println("Waiting for gateway IP");
+
+    // Wait for the Wi-Fi to assign a gateway ip-address
+    while (WiFi.gatewayIP() == "0.0.0.0");
+
+    // Setting global connection status to True
+    connected = true;
+
+    Serial.print("received gateway IP:");
+    Serial.println(WiFi.gatewayIP());
+    Serial.println();
+
+}
+
+// Function for connecting to the broker
+void connectBroker() {
+
+    // Since it is on a local network it is easier to get the gateway ip and connect to the broker using that
+    String gatewayIP = WiFi.gatewayIP().toString();
+    const char* broker = gatewayIP.c_str();
+
+    // Provide a username and password for authentication
+    mqttClient.setUsernamePassword(mqtt_user, mqtt_pass);
+
+    Serial.println("Attempting to connect to the MQTT broker.");
+
+    // Pre-define the port integer
+    int port     = 1883;
+
+    // Keep trying to connect to the broker
+    while (!mqttClient.connect(broker, port)) {
+        Serial.print("MQTT connection failed! Error code = ");
+        Serial.println(mqttClient.connectError());
+
+        // In case the broker won't connect due to a disconnected Wi-Fi run the function to reconnect
+        if (WiFi.status() != 0) {
+            connectWifi();
+        }
+    }
+
+    Serial.println("You're connected to the MQTT broker!");
+    Serial.println();
+}
 
 // Setup function for all the values and connections
 void setup() {
@@ -44,37 +105,17 @@ void setup() {
     while (!Serial) {
     }
 
-//    // Connect to Wi-Fi
-//    Serial.print("Attempting to connect to WPA SSID: ");
-//    Serial.println(ssid);
-//    while (WiFi.begin(ssid, pass) != WL_CONNECTED) {
-//        // failed, retry
-//        Serial.print(".");
-//        delay(5000);
-//    }
-//
-//    Serial.println("You're connected to the network");
-//    Serial.println();
-//
-//    // Provide a username and password for authentication
-//    mqttClient.setUsernamePassword(mqtt_user, mqtt_pass);
-//
-//    Serial.print("Attempting to connect to the MQTT broker.");
-//
-//    if (!mqttClient.connect(broker, port)) {
-//        Serial.print("MQTT connection failed! Error code = ");
-//        Serial.println(mqttClient.connectError());
-//
-//        while (1);
-//    }
-//
-//    Serial.println("You're connected to the MQTT broker!");
-//
-//    Serial.print("Waiting for messages on topic: ");
-//    Serial.println(subscribe_topic);
+    // Connect to Wi-Fi
+    connectWifi();
+
+    // Connect to MQTT broker
+    connectBroker();
 
     // Set function for receiving MQTT messages from the subscribed topics
     mqttClient.onMessage(eclipseReceived);
+
+    // Subscribe to topic for receiving information or commands
+    mqttClient.subscribe("/IO");
 }
 
 void loop() {
@@ -84,7 +125,7 @@ void loop() {
 
     // Checks if there are any messages from the MQTT
     // Also sends out a pulse so the connection will not be broken due to inactivity
-//    mqttClient.poll();
+    mqttClient.poll();
 
 
     // Read humidity
